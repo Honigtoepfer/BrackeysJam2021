72098538abdeb4744de14b027dd03c1777634271
Honigtoepfer <78220076+Honigtoepfer@users.noreply.github.com>
2022-02-20 17:10:19 +0100

    input

---
+++ Safe Valenzia/Assets/Game/Scripts/Player/PlayerExample.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerExample : MonoBehaviour
+{
+    
+    private MainInput mainUnput;
+    
+    void Awake()
+    {
+        mainUnput = new MainInput();
+        mainUnput.Player.interaction.started += ContextMenu => interaction();
+    }
+
+    private void interaction()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 dir = mainUnput.Player.move.ReadValue<Vector2>().normalized;
+    }
+
+    void OnEnable()
+    {
+        mainUnput.Enable();
+    }
+
+    void OnDisable()
+    {
+        mainUnput.Disable();
+    }
+
+}
